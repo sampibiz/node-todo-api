404682eaaec8b79eeacec16071b9763e6023beab
Boaz Shabon <boaz@sampi.co.il>
2018-08-17 12:23:40 +0300

    Update connection to the mongodb and set the node version

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require( 'mongoose' );
 
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://localhost:27017/TodoApp' );
+const URI = process.env.MONGODB_URI | 'mongodb://localhost:27017/TodoApp';
+mongoose.connect( URI );
 
 module.export = { mongoose };
