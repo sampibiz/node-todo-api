7b0650f8e1220bdf455f117130b7c673e8e07383
Boaz Shabon <boaz@sampi.co.il>
2018-08-18 17:45:31 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require( 'express' );
-var bodyParser = require( 'body-parser' );
+const _ = require( 'lodash' );
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 const {ObjectID} = require( 'mongodb' );
 
 var {mongoose} = require( './db/mongoose.js' );
@@ -73,11 +74,33 @@ app.delete( '/todos/:id', ( req, res ) => {
 });
 
 
-/*app.post( 'todos', ( reg, res ) => {
+app.patch( '/todos/:id', ( req, res ) => {
+    var id = req.params.id;
+    var body = _.pick( req.body, [ 'text', 'completed' ]);
 
+    if( !ObjectID.isValid( id ) ){
+        res.status( 404 ).send( );
+    }
+
+    if( _.isBoolean( body.completed ) && body.completed ) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate( id, { $set : body }, { new: true } ).then( ( todo ) => {
+        if( !todo ) {
+            return res.status( 400 ).send( );
+        }
+
+        res.status( 200 ).send( todo );
+    }).catch( ( err ) => {
+        res.status( 400 ).send( );
+    }) ;
 });
 
-app.post( 'todos', ( reg, res ) => {
+/* app.post( 'todos', ( reg, res ) => {
 
 });*/
 
